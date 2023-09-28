3a9e302510b7e92d1e7613dc47d61a76a87e6ecc
Jorge Ernesto <jeremigio2706@yahoo.com>
2023-09-28 02:09:44 -0400

    Agrega el endpoint /solution

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+# Implementacion de la función process_orders según las especificaciones
+def process_orders(orders, criterion):
+    if criterion == "completed":
+        filtered_orders = [order for order in orders if order["status"] == "completed"]
+    elif criterion == "pending":
+        filtered_orders = [order for order in orders if order["status"] == "pending"]
+    elif criterion == "canceled":
+        filtered_orders = [order for order in orders if order["status"] == "canceled"]
+    elif criterion == "all":
+        filtered_orders = orders
+    else:
+        raise HTTPException(status_code=400, detail="Criterio no válido")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    total_revenue = sum(order["price"] * order["quantity"] for order in filtered_orders)
+    return total_revenue
 
+# Implementacion del endpoint /solution
+@app.post("/solution")
+async def calculate_total_revenue(data: dict):
+    orders = data.get("orders", [])
+    criterion = data.get("criterion", "all")
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    if any(order["price"] < 0 for order in orders):
+        raise HTTPException(status_code=400, detail="El precio de un artículo no puede ser negativo")
+
+    total_revenue = process_orders(orders, criterion)
+    return {"total_revenue": total_revenue}
